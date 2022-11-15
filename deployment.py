a3f2b86f7f31edd9f91c9a9eb362d4fe69a11b63
Alex Stillman <alex.stillman@sky.com>
2022-11-15 16:25:59 +0000

    Now with github storage

--- deployment.py
+++ deployment.py
@@ -1,10 +1,14 @@
 from print_flow import print_flow
 from prefect.deployments import Deployment
+from prefect.filesystems import GitHub
+
+github_block = GitHub.load("alex-repo")
 
 deployment = Deployment.build_from_flow(
     flow=print_flow,
     name="print-simple",
     work_queue_name="default",
+    storage=github_block
 )
 
 deployment.apply()
